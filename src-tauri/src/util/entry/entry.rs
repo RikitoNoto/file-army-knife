3fe74bfe54bc87e3628739d2f3c0a8d33f1ee41e
R.Noto <rikitonoto@gmail.com>
2022-12-04 23:55:25 +0900

    create entry test base

--- src-tauri/src/util/entry/entry.rs
+++ src-tauri/src/util/entry/entry.rs
@@ -1,12 +1,13 @@
 
 pub mod entry{
 
+  use std::path::Path;
   pub struct ExploreInfo{
     pub path: String,
   }
 
-  pub fn enumrate_file(info: &ExploreInfo){
-
+  pub fn enumrate_file(info: &ExploreInfo) -> Vec<&Path>{
+    Vec::<&Path>::new()
   }
 }
 
@@ -20,25 +21,48 @@ mod tests{
 
   use std::fs;
   use std::path::Path;
-
-  use crate::util::entry::entry;
+  use super::entry;
 
   speculate!{
+    use super::entry::ExploreInfo;
+    use super::entry::enumrate_file;
+
     describe "enumrate_file test"{
       before{
+        // create temp directory.
         if !Path::new("temp").exists(){
-          fs::create_dir("temp");
+          if let Err(_) = fs::create_dir("temp") {
+            assert!(false);
+          }
         }
       }
 
       after{
         if Path::new("temp").exists(){
-          // fs::remove_dir_all("temp");
+          // remove temp directory.
+          if let Err(_) = fs::remove_dir_all("temp") {
+            assert!(false);
+          }
         }
       }
 
-      it "text"{
+      it "should be get no file when dir is empty"{
+        let info = ExploreInfo{
+          path: "temp".to_string(),
+        };
+        let path_vec = enumrate_file(&info);
+        assert_eq!(path_vec.len(), 0);
+      }
 
+      it "should be get one file when dir has a file"{
+        fs::File::create("temp/temp.a");
+        let info = ExploreInfo{
+          path: "temp".to_string(),
+        };
+
+        let path_vec = enumrate_file(&info);
+        assert_eq!(path_vec.len(), 1);
+        assert_eq!(path_vec[0].display().to_string(), "temp/temp.a");
       }
     }
 
